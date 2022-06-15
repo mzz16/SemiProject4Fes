dbdfc3734abbf1e0cd1f473560dd03c0a2d3b590
minji <busy8863@naver.com>
2022-06-15 18:56:37 +0900

    [mj] DB생성 But 슬라이드와 연동 실패

--- src/main/java/com/mj/festival/festival.sql
+++ src/main/java/com/mj/festival/festival.sql
@@ -3,11 +3,20 @@ create table festival_list(
 	codename varchar2 (500 char) not null,
 	guname varchar2 (500 char) not null,
 	title varchar2 (500 char) not null,
-	date varchar2 (500 char) not null,
+	fdate varchar2 (500 char) not null,
 	place varchar2 (500 char) not null,
-	orglink varchar2 (500 char) not null,
+	program varchar2 (600 char),
+	orglink varchar2 (500 char),
 	mainimg varchar2 (500 char) not null,
 	usetarget varchar2 (500 char) not null,
-	usefee varchar2 (500 char) not null
+	usefee varchar2 (500 char)
 	
 );
+
+create sequence festival_list_seq;
+
+drop sequence festival_list_seq;
+
+select * from festival_list;
+
+drop table festival_list CASCADE CONSTRAINT PURGE;
