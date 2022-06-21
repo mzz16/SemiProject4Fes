e446cb80bf36f31d453367ec29fdcdc0e1f40e30
siwonpark <soobin414@gmail.com>
2022-06-21 18:23:06 +0900

    [sw] 지도찾기에서 다른페이지로 넘어갈때 404 오류 수정

--- src/main/java/com/sw/main/searchMapHC.java
+++ src/main/java/com/sw/main/searchMapHC.java
@@ -1,5 +1,6 @@
 package com.sw.main;
 
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,19 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class searchMapHC
- */
 @WebServlet("/searchMapHC")
 public class searchMapHC extends HttpServlet {
-   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("contentPage", "SW_mapFolder/sw_map.jsp");
+		
+		
+		request.setAttribute("contentPage", "SW_mapFolder/sw_map3.jsp");
+		
 		request.getRequestDispatcher("index.jsp").forward(request, response);
-	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	
 	}
 
 }
