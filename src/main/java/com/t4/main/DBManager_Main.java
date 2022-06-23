52cfea9bf7b793fb951b1d2fba91752dae44406c
siwonpark <soobin414@gmail.com>
2022-06-23 18:54:31 +0900

    [sw] 클러스터 작업 json파일생성

--- src/main/java/com/t4/main/DBManager_Main.java
+++ src/main/java/com/t4/main/DBManager_Main.java
@@ -15,9 +15,9 @@ public class DBManager_Main {
 		// yj
 		//String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=/Users/allmight/eclipse-workspace/OracleWallet/Wallet_DB202204301707";
 		// mj
-		String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=C:/mj/Wallet_DB202204301707";
+	//	String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=C:/mj/Wallet_DB202204301707";
 		// sw
-	//	String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=C:/siwonjava/Wallet_DB202204301707";
+	String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=C:/siwonjava/Wallet_DB202204301707";
 		// dh
 	//	String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=/Users/mitakeran/LDH/Wallet_DB202204301707";
 		return DriverManager.getConnection(url, "YJJ", "YJ802soldesk");
