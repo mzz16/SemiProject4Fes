5d4fd6dc48ac5b85cd2343e6eebc085a85b63821
siwonpark <soobin414@gmail.com>
2022-06-27 09:33:21 +0900

    [sw] 지도 수정,클릭 중복이슈해결

--- src/main/java/com/t4/main/DBManager_Main.java
+++ src/main/java/com/t4/main/DBManager_Main.java
@@ -28,9 +28,9 @@ public class DBManager_Main {
 		// mj
 		//String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=C:/mj/Wallet_DB202204301707";
 		// sw
-		//String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=C:/siwonjava/Wallet_DB202204301707";
+		String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=C:/siwonjava/Wallet_DB202204301707";
 		// dh
-		String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=/Users/mitakeran/LDH/Wallet_DB202204301707";
+		//String url = "jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=/Users/mitakeran/LDH/Wallet_DB202204301707";
 		System.out.println("연결요청");
 		try {
 			return DriverManager.getConnection(url, "YJJ", "YJ802soldesk");
